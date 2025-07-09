17112614624ef5b10bcae38cbdd928733c21eb15
akashvivian <sakashvivian@gmail.com>
2025-07-09 09:25:31 +0530

    Update string declaration.java

--- string declaration.java
+++ string declaration.java
@@ -1,8 +1,15 @@
 public class Main
 {
 	public static void main(String[] args) {
-	    String name;
+	    String name,stname,sname,cname;
 	    name="nithin";
+	    stname="thiyagaraja nagar";
+	    sname="delhi";
+	    cname="australia";
 		System.out.println("name:"+name);
+		System.out.println("street name:"+stname);
+		System.out.println("state name:"+sname);
+		System.out.println("country name:"+cname);
+
 	}
 }
