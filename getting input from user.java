e56e7da9796df5ce1c41c315f62b41c3063c6bf0
akashvivian <sakashvivian@gmail.com>
2025-07-09 10:13:00 +0530

    Create getting input from user.java

---
+++ getting input from user.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Scanner stringScanner= new Scanner(System.in); 
+		String name,street,city,country;
+		name="Pranaw";
+		street="anna salai";
+		city="chennai";
+		country=stringScanner.nextLine();
+		System.out.println("Name ="+name+"\n"+"Street ="+street+"\n"+"city ="+city+"\n"+"country ="+country+"\n");
+	}
+}
